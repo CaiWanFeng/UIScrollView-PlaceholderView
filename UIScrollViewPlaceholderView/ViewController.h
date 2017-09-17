79a5403eb925d07ce492de57e95aeff848d4ad1c
caiqiang <caiqiang@kuaijiankang.com>
2017-09-17 16:24:26 +0800

    Initial Commit

---
+++ UIScrollViewPlaceholderView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIScrollViewPlaceholderView
+//
+//  Created by 蔡强 on 2017/9/17.
+//  Copyright © 2017年 kuaijiankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
